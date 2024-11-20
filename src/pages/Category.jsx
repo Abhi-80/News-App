1798a4491e4a05a31c9e55ec15063fce885bdcfa
अभिषेक कुमार ठाकुर <131022199+Abhi-80@users.noreply.github.com>
2024-11-20 14:15:24 +0530

    Update Category.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,31 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import { getCategoryNewsStart } from '../redux/action/news.action';
 import News from '../components/News';
 
-
-
 export default function Category() {
-  let { slug } = useParams();
-
-  const categoryNews = useSelector(state => state.categoryNews);
-
+  const { slug } = useParams();
+  const categoryNews = useSelector((state) => state.categoryNews);
   const dispatch = useDispatch();
 
-  useEffect(()=> {
-    dispatch(getCategoryNewsStart(slug))
-  }, [slug, categoryNews.length])
+  useEffect(() => {
+    dispatch(getCategoryNewsStart(slug));
+  }, [dispatch, slug]);
 
   return (
-    <div className='container'>
+    <div className="container">
       <div className="row">
-      {
-        categoryNews.length > 0 && categoryNews.map((item,index) => (
-          <News item={item} key={index}/>
-        ))
-      }
+        {categoryNews.length > 0 ? (
+          categoryNews.map((item) => (
+            <News key={item.id} item={item} />
+          ))
+        ) : (
+          <div className="col-12 text-center">No news available for this category.</div>
+        )}
       </div>
     </div>
-  )
+  );
 }
